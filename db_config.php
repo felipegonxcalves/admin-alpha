016f58a0a3439c93aff031bb617f09e52f977380
Felipe Gonçalves <salvandofelipe@hotmail.com>
2020-03-26 19:38:03 -0300

    Correção Utf mysql

--- db_config.php
+++ db_config.php
@@ -1,3 +1,3 @@
 <?php
 
-    return new \PDO('mysql:host=mysql.perfil.alphage.com.br;dbname=perfil', 'perfil', 'jlkb3302', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    return new \PDO('mysql:host=mysql.perfil.alphage.com.br;dbname=perfil;charset=utf8', 'perfil', 'jlkb3302', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8']);
